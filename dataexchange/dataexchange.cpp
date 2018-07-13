86cf9d0140b6608282398282e930465b47bc68b9
wuxingyi <wuxingyi2015@outlook.com>
2018-07-13 11:07:53 +0800

    use owner to track market info
    Signed-off-by: wuxingyi <wuxingyi2015@outlook.com>

--- dataexchange/dataexchange.cpp
+++ dataexchange/dataexchange.cpp
@@ -16,50 +16,67 @@ class dataexchange : public contract {
             _markets(_self, _self){}
 
         // @abi action
-        void removemarket(uint64_t id){
-            require_auth(_self);
+        void removemarket(account_name owner, uint64_t id){
+            //only the market owner can create a market
+            require_auth(owner);
 
             auto iter = _markets.find(id);
             eosio_assert(iter != _markets.end(), "marketname not have been created yet");
-
+            eosio_assert(iter->mowner == owner , "market does not belong to you");
+            
             _markets.erase(iter);
         }
 
 
         // @abi action
-        void createmarket(uint64_t marketname){
-            //only the owner can create a market
+        void createmarket(account_name owner, uint64_t type, string desp){
+            //only the contract owner can create a market
             require_auth(_self);
-            //eosio_assert(marketname.length() < 30, "marketname should be less than 30 characters");
-            eosio_assert(has_market(marketname) == false, "marketname already exists");
+
+            eosio_assert(desp.length() < 30, "marketname should be less than 30 characters");
+            eosio_assert(hasmarket_byaccountname(owner) == false, "an account can only create only one market now");
+            eosio_assert((type > typestart && type < typeend), "out of market type");
 
             auto newid  = _markets.available_primary_key();
             _markets.emplace( _self, [&]( auto& row) {
                 row.marketid = newid;
-                row.marketname = marketname;
+                row.mowner = owner;
+                row.mtype = type;
+                row.mdesp = desp;
             });
         }
 
     private:
+        static const uint64_t typestart = 0;
+        static const uint64_t authorities = 1;
+        static const uint64_t shopping = 2;
+        static const uint64_t travelling = 3;
+        static const uint64_t sellouts = 4;        
+        static const uint64_t typeend = 5;
+
         // @abi table datamarkets i64
         struct datamarkets {
             uint64_t marketid; 
-            uint64_t marketname;
+            uint64_t mtype;
+            string mdesp;
+            account_name mowner;
 
-            uint64_t by_marketname() const { return marketname; }
             uint64_t primary_key() const { return marketid; }
-            EOSLIB_SERIALIZE( datamarkets, (marketid)(marketname))
+            uint64_t by_mtype() const { return mtype; }
+            uint64_t by_mowner() const { return mowner; }
+            EOSLIB_SERIALIZE( datamarkets, (marketid)(mtype)(mdesp)(mowner))
         };
 
 
-        bool has_market( uint64_t market)const {
-           auto idx = _markets.template get_index<N(marketname)>();
-           auto itr = idx.find(market);
+        bool hasmarket_byaccountname( account_name accnt)const {
+           auto idx = _markets.template get_index<N(mowner)>();
+           auto itr = idx.find(accnt);
            return itr != idx.end();
         }
 
         multi_index <N(datamarkets), datamarkets, 
-                    indexed_by< N(marketname), const_mem_fun<datamarkets, uint64_t, &datamarkets::by_marketname> >
+                    indexed_by< N(mtype), const_mem_fun<datamarkets, uint64_t, &datamarkets::by_mtype> >,
+                    indexed_by< N(mowner), const_mem_fun<datamarkets, uint64_t, &datamarkets::by_mowner> >
         > _markets;
 };
 
---
+++ dataexchange/testCreate100Markets.py
@@ -0,0 +1,36 @@
+import string
+import sys
+import time
+import os
+import random
+import json
+import commands
+
+
+new_words = []
+def make_new_words(count):
+    for i in range(count):
+        w = ''.join(random.choice(string.ascii_letters).lower() for m in range(10))
+        new_words.append(w)
+
+
+def createAccounts():
+    # in my system keosd is runing at 8889 port
+    for i in new_words:
+        r = os.system('cleos --wallet-url=http://127.0.0.1:8889 create account eosio %s EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV' % i)
+
+def Create100Markets():
+    for i in new_words:
+        s={'owner': 'asdf', 'type': 2, 'desp':'taobao.com'}
+        s['owner'] = i
+        s2 = json.dumps(s)
+        ss = "cleos --wallet-url=http://127.0.0.1:8889 push action dddd createmarket " +  "' " + s2 + " '" + " -p dddd" 
+        print ss
+        os.system(ss)
+
+
+
+if __name__ == "__main__":
+    make_new_words(100)
+    createAccounts()
+    Create100Markets()
