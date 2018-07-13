45bb28511858b0055f0be4964d33345256172fc4
wuxingyi <wuxingyi2015@outlook.com>
2018-07-13 19:04:05 +0800

    add order operation abis createorder/cancelorder
    Signed-off-by: wuxingyi <wuxingyi2015@outlook.com>

--- dataexchange/README.md
+++ dataexchange/README.md
@@ -17,6 +17,17 @@ cleos get table dddd dddd datamarkets -l -1
 ## remove market
 removing market need both owner's permission and the marketid.
 note that only the market owner can remove a market.
+
 ```
 cleos push action dddd removemarket '["bdibxtljzc",104]' -p bdibxtljzc
+```
+
+## create a order with a giving marketid:
+```
+cleos push action dddd createorder '["eosio","bdibxtljzc", 0, 2,"jd.com"]' -p eosio
+```
+
+## remove previously created order
+```
+cleos push action dddd cancelorder '["eosio",0]' -p eosio
 ```
--- dataexchange/dataexchange.cpp
+++ dataexchange/dataexchange.cpp
@@ -13,7 +13,8 @@ class dataexchange : public contract {
     public:
         dataexchange( account_name self ) :
             contract(self),
-            _markets(_self, _self){}
+            _markets(_self, _self),
+            _askingorders(_self, _self){}
 
         // @abi action
         void removemarket(account_name owner, uint64_t id){
@@ -46,6 +47,33 @@ class dataexchange : public contract {
             });
         }
 
+        // @abi action
+        //(fixme) marketowner should be removed
+        void createorder(account_name seller, account_name marketowner, uint64_t marketid, uint64_t price, string dataforsell) {
+            require_auth(seller);
+
+            eosio_assert(hasmareket_checkouidandowner(marketid, marketowner) == true, "no such market");
+
+            auto newid  = _askingorders.available_primary_key();
+            // we can only put it to the contract owner  scope
+            _askingorders.emplace( _self, [&]( auto& order) {
+                order.orderid = newid;
+                order.seller = seller;
+                order.marketid = marketid;
+                order.price = price;
+                order.dataforsell = dataforsell;
+            });
+        }
+
+        void cancelorder(account_name seller, uint64_t orderid) {
+            require_auth(seller);
+            auto iter = _askingorders.find(orderid);
+
+            eosio_assert(iter != _askingorders.end() , "no such order");
+            eosio_assert(iter->seller == seller, "order doesn't belong to you");
+            _askingorders.erase(iter);
+        }
+
     private:
         static const uint64_t typestart = 0;
         static const uint64_t authorities = 1;
@@ -67,6 +95,12 @@ class dataexchange : public contract {
             EOSLIB_SERIALIZE( datamarket, (marketid)(mtype)(mdesp)(mowner))
         };
 
+        // because marketid maybe be reclaimed after calling removemarket
+        bool hasmareket_checkouidandowner(uint64_t id, account_name owner)const {
+           auto iter = _markets.find(id);
+           if (iter == _markets.end() || iter->mowner != owner) return false;
+           return true;
+        }
 
         bool hasmarket_byaccountname( account_name accnt)const {
            auto idx = _markets.template get_index<N(mowner)>();
@@ -78,6 +112,26 @@ class dataexchange : public contract {
                     indexed_by< N(mtype), const_mem_fun<datamarket, uint64_t, &datamarket::by_mtype> >,
                     indexed_by< N(mowner), const_mem_fun<datamarket, uint64_t, &datamarket::by_mowner> >
         > _markets;
+
+        //@abi table askingorders i64
+        struct askingorder {
+            uint64_t orderid;     //orderid is the primary key for quick erase of pening asking orders
+            uint64_t marketid; 
+
+            //account of the seller
+            account_name seller;
+            uint64_t price;     //(fixme) use asset type
+            string dataforsell; //(fixme) need change
+
+            uint64_t primary_key() const { return orderid; }
+            uint64_t by_seller() const { return seller; }
+            EOSLIB_SERIALIZE( askingorder, (orderid)(marketid)(seller)(price)(dataforsell))
+        };
+
+
+        multi_index <N(askingorders), askingorder ,
+                     indexed_by< N(seller), const_mem_fun<askingorder, uint64_t, &askingorder::by_seller> >
+        > _askingorders;
 };
 
-EOSIO_ABI( dataexchange, (createmarket)(removemarket))
+EOSIO_ABI( dataexchange, (createmarket)(removemarket)(createorder)(cancelorder))
